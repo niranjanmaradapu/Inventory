dc1b4c1b0f1ba4aaac1078fae8b148ed131f506b
vasavi yakkali <vasavi.yakkali@otsi.co.in>
2022-06-17 14:05:55 +0530

    change code in AuditAwareImpl class

--- inventory-module/src/main/java/com/otsi/retail/inventory/config/AuditAwareImpl.java
+++ inventory-module/src/main/java/com/otsi/retail/inventory/config/AuditAwareImpl.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuditAwareImpl implements AuditorAware<Long> {
 
+	private final Logger log = LogManager.getLogger(AuditAwareImpl.class);
+
 	@Autowired
 	private HttpServletRequest request;
 
 	@Override
 	public Optional<Long> getCurrentAuditor() {
-		String userId = request.getHeader("userId");
+		String userId = null;
+		try {
+			userId = request.getHeader("userId");
+		} catch (Exception ex) {
+			log.error("exception in current auditor " + ex);
+		}
 		if (StringUtils.isNotBlank(userId)) {
 			return Optional.of(Long.valueOf(userId));
 		}
